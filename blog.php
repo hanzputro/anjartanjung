b227146f30262099e1dd855363fb9251a7a7e260
hanif putra <hanzputro@gmail.com>
2015-04-06 22:28:06 +0700

    up0.5
    - blog

---
+++ blog.php
@@ -0,0 +1,32 @@
+
+<?php include("includes/header.php"); ?>
+	
+	<div class="main__wrapper">
+		<div class="post__blog">
+    		<ul class="post__blog--ul">
+    			<!-- post content -->
+    			<li class="post__blog--li">
+    				<h2 class="lt">James & Rita</h2>
+    				<small class="post__date">2 December 2014</small>
+    				<ul class="post__img--ul">
+    					<li class="post__img--li">
+    						<img src="" alt="" class="post__img--img">
+    					</li>
+    					<li class="post__img--li">
+    						<img src="" alt="" class="post__img--img">
+    					</li>
+    				</ul>
+    				<p>
+		    			Currently based in tropical island Bali, I specialize in destination weddings worldwide and will gladly travel to any location for a wedding celebration.<br>
+						Photography is a way of feeling, of touching, of loving, waht you have caught on film is captured forever...<br>
+		    			it remembers little things, long after you have forgotten everything.<br>
+		    		</p>
+    			</li>
+    		</ul>    		
+		</div>    	
+	</div>
+
+</div><!-- /container -->
+		
+<?php include("includes/footer.php"); ?>
+		
