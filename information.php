56de6a6b4ed10b9e4c078673617f933ec71ee2db
hanif putra <hanzputro@gmail.com>
2015-04-11 15:10:44 +0700

    Revert "revtheme0.9"
    This reverts commit bfb7c751ccfd19b2fc36b12640308cad926e6a17.

--- information.php
+++ information.php
@@ -1,18 +1,13 @@
-<?php
-/*
-Template Name: Information Page
-*/
-?>
 
-<?php get_header(); ?>
-
+<?php include("includes/header.php"); ?>
+	
     <div class="main__wrapper">
         <div class="information">
             <div class="information__left">
-                <img class="information__left--img" src="<?php bloginfo('template_url'); ?>/assets/images/aboutus_left-img.jpg" alt="">
+                <img class="information__left--img" src="assets/images/aboutus_left-img.jpg" alt="">
             </div>
             <div class="information__right">
-                <!-- <h1 class="lt">About Us</h1>
+                <h1 class="lt">About Us</h1>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lectus nibh, 
                   tincidunt sed libero at, venenatis blandit elit. Pellentesque habitant morbi 
@@ -37,28 +32,12 @@ Template Name: Information Page
                   Praesent nec est nibh. Integer vel dolor leo. Nam elementum accumsan 
                   justo sed ultrices. Donec auctor enim a mi pretium, tincidunt imperdiet
                   eros tristique. Praesent scelerisque eu tortor nec volutpat. 
-                </p> -->
-
-                <ul>
-                <?php
-
-
-                $args = array( 'posts_per_page' => 5, 'offset'=> 1, 'category' => 1 );
-
-                $myposts = get_posts( $args );
-                foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-                  <li>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                  </li>
-                <?php endforeach; 
-                wp_reset_postdata();?>
-
-                </ul>
+                </p>
             </div>
         </div>      
     </div>
     <br>
 </div><!-- /container -->
 		
-<?php get_footer(); ?>
+<?php include("includes/footer.php"); ?>
 		
